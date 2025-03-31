70656d83b75b0ee3999fb1a85b003c6d32e4ca2f
quoctv <vanquoc26520@gmail.com>
2025-03-31 20:33:07 +0700

    Render videos page

--- app/Livewire/Videos.php
+++ app/Livewire/Videos.php
@@ -2,10 +2,17 @@
 
 namespace App\Livewire;
 
+use App\Models\Video;
 use Livewire\Component;
 
 class Videos extends Component
 {
+    public $videos = [];
+    public function mount()
+    {
+        $this->videos = Video::all();
+        // dd($this->videos);
+    }
     public function render()
     {
         return view('livewire.videos');
--- resources/views/livewire/videos.blade.php
+++ resources/views/livewire/videos.blade.php
@@ -573,89 +573,25 @@
                         <div class="list-blogs">
 
                             <div class="row">
-
+                                @foreach ($videos as $video)
                                 <div class="col-md-6">
                                     <div class="item-blog item-blog-project position-relative rounded-10">
                                         <a class="thumb d-block ratio3by2 position-relative"
-                                            href="https://www.youtube.com"
-                                            title="Mang phong cách hiện đại, tối giản vào căn nhà">
+                                            href="{{ $video->url }}"
+                                            title="{{ $video->title }}">
                                             <img class="position-absolute"
-                                                src="/assets/100/501/740/articles/9.jpg"
-                                                alt="Mang phong cách hiện đại, tối giản vào căn nhà">
-                                        </a>
+                                                src="{{ Storage::url($video->banner) }}"
+                                                alt="{{ $video->title }}">
                                         <div class="info position-absolute">
                                             <h3 class="font-weight-bold">
-                                                <a  href="https://www.youtube.com"
-                                                    title="Mang phong cách hiện đại, tối giản vào căn nhà">Mang phong
-                                                    cách hiện
-                                                    đại, tối giản vào căn nhà</a>
+                                                <a  href="{{ $video->url }}"
+                                                    title="{{ $video->title }}">{{ $video->title }}</a>
                                             </h3>
 
                                         </div>
                                     </div>
                                 </div>
-                                <div class="col-md-6">
-                                    <div class="item-blog item-blog-project position-relative rounded-10">
-                                        <a class="thumb d-block ratio3by2 position-relative"
-                                            href="https://www.youtube.com"
-                                            title="Mang phong cách hiện đại, tối giản vào căn nhà">
-                                            <img class="position-absolute"
-                                                src="/assets/100/501/740/articles/9.jpg"
-                                                alt="Mang phong cách hiện đại, tối giản vào căn nhà">
-                                        </a>
-                                        <div class="info position-absolute">
-                                            <h3 class="font-weight-bold">
-                                                <a  href="https://www.youtube.com"
-                                                    title="Mang phong cách hiện đại, tối giản vào căn nhà">Mang phong
-                                                    cách hiện
-                                                    đại, tối giản vào căn nhà</a>
-                                            </h3>
-
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="item-blog item-blog-project position-relative rounded-10">
-                                        <a class="thumb d-block ratio3by2 position-relative"
-                                            href="https://www.youtube.com"
-                                            title="Mang phong cách hiện đại, tối giản vào căn nhà">
-                                            <img class="position-absolute"
-                                                src="/assets/100/501/740/articles/9.jpg"
-                                                alt="Mang phong cách hiện đại, tối giản vào căn nhà">
-                                        </a>
-                                        <div class="info position-absolute">
-                                            <h3 class="font-weight-bold">
-                                                <a  href="https://www.youtube.com"
-                                                    title="Mang phong cách hiện đại, tối giản vào căn nhà">Mang phong
-                                                    cách hiện
-                                                    đại, tối giản vào căn nhà</a>
-                                            </h3>
-
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="item-blog item-blog-project position-relative rounded-10">
-                                        <a class="thumb d-block ratio3by2 position-relative"
-                                            href="https://www.youtube.com"
-                                            title="Mang phong cách hiện đại, tối giản vào căn nhà">
-                                            <img class="position-absolute"
-                                                src="/assets/100/501/740/articles/9.jpg"
-                                                alt="Mang phong cách hiện đại, tối giản vào căn nhà">
-                                        </a>
-                                        <div class="info position-absolute">
-                                            <h3 class="font-weight-bold">
-                                                <a  href="https://www.youtube.com"
-                                                    title="Mang phong cách hiện đại, tối giản vào căn nhà">Mang phong
-                                                    cách hiện
-                                                    đại, tối giản vào căn nhà</a>
-                                            </h3>
-
-                                        </div>
-                                    </div>
-                                </div>
-
-
+                                @endforeach
                             </div>
                             <div class="pagenav m-0">
 
